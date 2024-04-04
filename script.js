183423e24b5a1a91b46cab0ae4a9ebeccf096367
dkiltinas <kiltinasdeividas@gmail.com>
2024-04-04 18:24:07 +0300

    Mobile version : Header, hero, tab section added

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
-  console.log("flexing");
 }
 
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
-
-showSidebar();

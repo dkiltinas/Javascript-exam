ef418c0f28fe21acbdd825eb223ddfb262dc299e
dkiltinas <kiltinasdeividas@gmail.com>
2024-04-08 15:15:04 +0300

    Added tab content change on click.

--- script.js
+++ script.js
@@ -55,3 +55,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
   indicators[0].classList.add("active");
 });
+
+//============================Tab section ================================//
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tab");
+  const contents = [
+    {
+      title: "Register",
+
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, iusto? Officia obcaecati, animi officiis blanditiis accusantium voluptates impedit delectus, corrupti quasi reprehenderit enim vitae ex nemo rem laboriosam ipsa amet.",
+    },
+    {
+      title: "Apply",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui cupiditate quibusdam beatae molestias et assumenda repellat ut natus rerum repellendus quaerat aspernatur, nihil corporis totam a corrupti itaque enim! Alias!",
+    },
+    {
+      title: "Receive",
+
+      text: "lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita minima porro possimus explicabo, repellendus earum atque magni rem soluta veniam deleniti qui ipsam culpa sapiente ipsum? Veritatis perspiciatis at laborum.lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt tempora hic ipsum, accusamus doloribus odit? Maxime illo iusto ab, accusamus nam, voluptatum commodi alias ratione a voluptas fuga cumque sed.",
+    },
+  ];
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", function () {
+      const titleElement = document.querySelector(".text h4");
+      const textElement = document.querySelector(".text p");
+      const content = contents[index];
+
+      titleElement.textContent = content.title;
+      textElement.textContent = content.text;
+
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+
+      tab.classList.add("active");
+    });
+  });
+});

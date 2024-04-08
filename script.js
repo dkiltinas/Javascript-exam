cbc39f6a75cd03e6e0f616dfbac54acf515fa048
dkiltinas <kiltinasdeividas@gmail.com>
2024-04-08 13:02:34 +0300

    Added smooth scroll to section

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const targetId = event.target.getAttribute("href");
+
+  document.querySelector(targetId).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});

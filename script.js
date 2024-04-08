5c845f15d74c693a05f67697cacdebf52787b5ba
dkiltinas <kiltinasdeividas@gmail.com>
2024-04-08 14:32:00 +0300

    Added more reviews for testemonial section.  Also functional nav icons added, comments are now changing based on the pressed icon

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//==============================Scroll==============================//
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -20,4 +21,37 @@ function scrollToSection(event) {
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", scrollToSection);
+});
+
+//========================Review gallery============================//
+document.addEventListener("DOMContentLoaded", function () {
+  const reviews = document.querySelectorAll(".reviews");
+  const indicators = document.querySelectorAll(".indicator");
+
+  function showReviews(index) {
+    reviews.forEach(function (review) {
+      review.style.display = "none";
+    });
+
+    for (let i = index * 3; i < (index + 1) * 3; i++) {
+      if (reviews[i]) {
+        reviews[i].style.display = "flex";
+      }
+    }
+  }
+
+  showReviews(0);
+
+  indicators.forEach(function (indicator, index) {
+    indicator.addEventListener("click", function () {
+      indicators.forEach(function (indicator) {
+        indicator.classList.remove("active");
+      });
+      this.classList.add("active");
+
+      showReviews(index);
+    });
+  });
+
+  indicators[0].classList.add("active");
 });
